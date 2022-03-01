a3d735a7b93833c648f6eccaab3bc5f1a45fcacb
andreu casals <acasalsr@gmail.com>
2022-03-01 22:04:10 +0100

    redux installed

--- redux-count/src/App.js
+++ redux-count/src/App.js
@@ -1,5 +1,9 @@
 import "./index.css";
 import CounterUseReducer from "./CounterUseReducer";
+import {counter, increment, reset} from "./counter";
+
+import { createStore } from "redux";
+const store = createStore(counter, 0);
 
 export default function App() {
   return (
--- redux-count/src/index.js
+++ redux-count/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-console.log("🐕 🥔 ");
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
